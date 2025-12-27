613e641e2e288fa0a4e3f32ec0c86f04416184e8
Mahidhar5757 <168r1a04d9@gmail.com>
2025-12-27 08:14:24 +0530

    new Coommit

---
+++ new.py
@@ -0,0 +1,8 @@
+a = float(input("Enter a  number"))
+b = float(input("Enter a  number"))
+c = float(input("Enter a  number"))
+
+s = (a + b + c)/2
+
+Area_of_Traiangle= (s*(s-a)*(s-b)*(s-c))**0.5
+print(Area_of_Traiangle)
